e9c30af22907f6ba61d217b47ca368cd52a72d21
Fletchpike <Fletcherpike10@gmail.com>
2026-04-09 20:06:54 -0400

    Update EditorCommon.cs

--- Assets/Editor/Scripts/EditorCommon.cs
+++ Assets/Editor/Scripts/EditorCommon.cs
@@ -170,7 +170,6 @@ namespace Fletchpike.Editor
             return EditorGUIUtility.singleLineHeight;
         }
     }
-    [CanEditMultipleObjects]
     [CustomEditor(typeof(AudioContainer))]
     public class AudioContainerEditor : UnityEditor.Editor
     {
